e814487d81686b95586762e995164077b3f4b3b6
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-08-07 08:18:24 -0500

    fixed some bugs

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const db = require('../models');
+const Workout = require('../model/workout');
 
 mongoose.connect('mongodb://localhost/workout', {
   useNewUrlParser: true,
