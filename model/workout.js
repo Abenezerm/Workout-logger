6a5c06eb4b2d85afc96db83ae41336cba31cecef
Abenezer Merdassa <abenezermerdassa@gmail.com>
2021-08-08 03:55:09 -0500

    Update workout.js

--- model/workout.js
+++ model/workout.js
@@ -43,7 +43,7 @@ const workoutSchema = new Schema({
 });
 
 //dynamic propeties fro schema...
-WorkoutSchema.virtual('totalDuration').get(function () {
+workoutSchema.virtual('totalDuration').get(function () {
     return this.exercises.reduce((total, exercise) => {
         return total + exercise.duration;
     }, 0);
